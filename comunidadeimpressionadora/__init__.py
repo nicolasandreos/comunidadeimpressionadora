71c8ad901d0d400a9063c2986b6ef67faacb7106
Nicolas <104907066+nicolasandreos@users.noreply.github.com>
2023-12-08 18:14:04 -0300

    Update __init__.py

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -2,13 +2,17 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 lista_usuarios = ['Nicolas', 'Nicole', 'Marcia', 'Deived']
 
 app.config['SECRET_KEY'] = 'jdasndjbasdgyq4723ryg22525f'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
-
+if os.getenv('DATABASE_URL'):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+    
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
@@ -16,4 +20,3 @@ login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
 
 from comunidadeimpressionadora import routes
-
